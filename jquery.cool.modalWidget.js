6cddf8b5065c1e61dded12e45fdfb8778ecf22f6
Paris <ptheofan@gmail.com>
2014-02-07 17:50:03 +0100

    Update jquery.cool.modalWidget.js
    couple of bug fixes

--- jquery.cool.modalWidget.js
+++ jquery.cool.modalWidget.js
@@ -2,15 +2,15 @@
  * cool.modalWidget
  * @author: Paris Theofanidis (ptheofan@gmail.com)
  * @source: git@github.com:ptheofan/cool.modalWidget.git
- * 
+ *
  * Will convert a div to modal popup (but it's not a dialog).
  * This is based on the CSS facebook (which appearently 'borrowed' it from google)
  * uses to render the modal popups which show case photos, etc.
- * 
+ *
  * Currently tested with Chrome and Firefox. If you happen to have IE and try it
  * let me know if it worked. Should you apply any IE patches also send 'em over
  * to publish them (under your name of course) with the widget
- * 
+ *
  * It beats jDialog because
  *   1. It's hellable lighter
  *   2. Aligns itself at screen center ALWAYS (with pure css!) (unline my jDialog extension that uses js)
@@ -22,62 +22,62 @@
  *   8. Has an event for pretty much everything :D
  *   9. Does NOT restrict your modal layout in any way (no title, no X button, no nothing!) :)
  *  10. It can mimic the layout of jDialog allowing you use the most important aspects of it for styling
- *  
+ *
  *  Configuration Options
  *    autoOpen: false
  *    if true will launch your modal the moment you create it
- *      
+ *
  *    removeElementOnDestroy: false
  *    if true will REMOVE your element from DOM when modal is destroyed
- *      
+ *
  *    closeOnEscape: false
  *    if true will close the modal when ESC is pressed (actually code borrowed from jDialog ;)
- *    
+ *
  *    destroyOnClose: false
  *    if true will destroy the widget automagically when modal closes
  *    FYI: if removeElementOnDestroy is set to true it will also destroy your element! :)
- *    
+ *
  *    css: {blocker: {...}, hoder: {...}, alignment: {...}, container: {...}}
  *    contains the CSS that is applied to the divs that wrap your element.
  *    FYI: z-index and transparency level of the blocker can be easily adjusted
  *    from the css.blocker['z-index'] and css.blocker['background-color'].
  *    [HINT] You can even add a png bg with your logo, cool right? ;)
- *    
- *    
+ *
+ *
  *  Functions (methods)
  *    open (events: beforeOpen, afterOpen)
  *    will launch the modal
- *    
+ *
  *    close (events: beforeClose, afterClose)
  *    will close the modal
- *    
+ *
  *    destroy (events: beforeDestroy)
  *    will destroy the widget
- *    
+ *
  *    content (events: beforeContentChange, afterContentChange)
  *    1st argument: HTML or jQuery element
  *    will swap the current content with the provided content (accepts one argument)
  */
 (function($){
     $.widget('cool.modalWidget', {
-        
+
         options: {
             // Open the moment plugin binds to element
             autoOpen: false,
-            
+
             // Destroy the element when modal closes
             removeElementOnDestroy: false,
-            
+
             // Dismiss modal on escape
             closeOnEscape: false,
-            
+
             // Destroy the widget when modal is closed (will chain with closeOnEscape and destroyElement)
             destroyOnClose: false,
-            
+
             // Mimic the jDialog divs layout
             mimicJDialog: false,
-            
-            
+
+
             // Classes to attach to the respective div
             classes: {
                 blocker: null,
@@ -92,7 +92,7 @@
                 jDialogTitleClose: 'ui-dialog-titlebar-close ui-corner-all',
                 jDialogTitleCloseSpan: 'ui-icon ui-icon-closethick'
             },
-            
+
             // The CSS that is applied to the divs that will contain the element
             css: {
                 blocker: {
@@ -104,15 +104,14 @@
                     'overflow': 'hidden',
                     'z-index': '1200',
                     'background-color': 'black',
-                    'opacity': 0.84,
+                    'opacity': 0.75,
                     '-ms-filter': '"progid:DXImageTransform.Microsoft.Alpha(Opacity=84)"',
-                    'filter': 'alpha(opacity=84)',
-                    '-moz-opacity': 0.84,
-                    '-khtml-opacity': 0.84,
-//                    'background': 'URL(/css/bg1.png) repeat',
+                    'filter': 'alpha(opacity=75)',
+                    '-moz-opacity': 0.75,
+                    '-khtml-opacity': 0.75,
                     'display': 'none'
                 },
-                
+
                 positioner: {
                     'position': 'fixed',
                     'top': '0px',
@@ -125,14 +124,14 @@
                     'overflow-y': 'scroll',
                     'display': 'none'
                 },
-                
+
                 holder: {
                     'display': 'table',
                     'height': '100%',
                     'width': '100%',
                     'table-layout': 'fixed'
                 },
-                
+
                 alignment: {
                     'display': 'table-cell',
                     'text-align': 'center',
@@ -140,7 +139,7 @@
                     'width': '100%',
                     'height': '100%'
                 },
-                
+
                 container: {
                     'position': 'relative',
                     'display': 'inline-block',
@@ -150,7 +149,7 @@
                 }
             }
         },
-        
+
         // Divs we wrap around element
         _blocker: null,
         _positioner: null,
@@ -161,7 +160,7 @@
         _bodyOldOverflowX: null,
         _bodyOldOverflowY: null,
 
-        
+
         _create: function() {
             this._self = this;
             this._blocker = $('<div>').css(this.options.css.blocker).addClass(this.options.classes.blocker);
@@ -169,7 +168,7 @@
             this._holder = $('<div>').css(this.options.css.holder).addClass(this.options.classes.holder);
             this._alignment = $('<div>').css(this.options.css.alignment).addClass(this.options.classes.alignment);
             this._container = $('<div>').css(this.options.css.container).addClass(this.options.classes.container);
-            
+
             // Nest the divs and grab the element
             this._positioner.append(this._holder.append(this._alignment.append(this._container.wrapInner(this.element))));
 
@@ -208,15 +207,15 @@
             $('body').append(this._blocker);
             $('body').append(this._positioner);
         },
-        
-        
+
+
         _init: function() {
             if (this.options.autoOpen === true) {
                 this.open();
             }
         },
-        
-        
+
+
         /**
          * Open the modal
          */
@@ -246,21 +245,30 @@
          */
         close: function(evt) {
             if (this._trigger('beforeClose', evt)) {
+                $('body').css({'overflow-x': this._bodyOldOverflowX, 'overflow-y': this._bodyOldOverflowY});
+                this._trigger('afterClose');
+
                 if (this.options.closeOnEscape === true) {
                     this._positioner.unbind('keypress');
                 }
                 if (this.options.destroyOnClose === true) {
-                    this.destroy();
+                    this._positioner.remove();
+                    this._blocker.remove();
+
+                    // Restore elemet on DOM (if destroyOnHide = false)
+                    if (this.options.removeElementOnDestroy === false) {
+                        $('body').append(this.element);
+                    } else {
+                        this.element.remove();
+                    }
                 } else {
                     this._positioner.css({'display': 'none'});
                     this._blocker.css({'display': 'none'});
-                    $('body').css({'overflow-x': this._bodyOldOverflowX, 'overflow-y': this._bodyOldOverflowY});
-                    this._trigger('afterClose');
                 }
             }
         },
-        
-        
+
+
         /**
          * Replace the contents of the modal with whatever you want.
          * FYI: Your associated element will loose all its previous
@@ -269,24 +277,6 @@
             if (this._trigger('beforeContentChange')) {
                 this.element.html($(elem));
                 this._trigger('afterContentChange');
-            }
-        },
-        
-        
-        /**
-         * Remove the widget
-         */
-        destroy: function() {
-            if (this._trigger('beforeDestroy')) {
-                // Restore elemet on DOM (if destroyOnHide = false)
-                if (this.options.removeElementOnDestroy === false) {
-                    $('body').append(this.element);
-                } else {
-                    this.element.remove();
-                }
-
-                // Destroy blocker and everything in it
-                this._positioner.remove();
             }
         }
     });
